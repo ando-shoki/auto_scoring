b822815383f10c7ab3d95d65ac087d158197a45c
AitM <mahara.aito.den@gmail.com>
2019-12-15 00:31:46 +0900

    問題番号、解答、正答の読み取り結果を表示

--- final.py
+++ final.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask, request, redirect, url_for, render_template, flash
 from werkzeug.utils import secure_filename
 from keras.models import Sequential, load_model
-from keras.preprocessing import image
+# from keras.preprocessing import image
+import keras.preprocessing.image
 #from tensorflow.keras.initializers import GlorotUniform
 import tensorflow as tf
 import numpy as np
@@ -11,9 +12,10 @@ import matplotlib.pyplot as plt
 import pyzbar.pyzbar as pyzbar
 from pyzbar.pyzbar import decode
 from PIL import Image, ImageDraw, ImageFont
+from keras_preprocessing import image
 
 #model = load_model('auto_scoring/model.h5')#学習済みモデルをロードする
-model = tf.keras.models.load_model('model.h5')#学習済みモデルをロードする
+model = tf.keras.models.load_model('model_mnist.h5')#学習済みモデルをロードする
 
 app = Flask(__name__)
 
@@ -52,9 +54,11 @@ def upload_file():
             img_file.save(os.path.join(UPLOAD_FOLDER,img_filename))
             img_filepath = os.path.join(UPLOAD_FOLDER, img_filename)
             
-            qr_img = image.load_img(img_filepath ,grayscale=True,target_size=(image_size,image_size))
-            gray_img = cv2.cvtColor(qr_img, cv2.COLOR_BGR2GRAY)
-            blurred_img = cv2.GaussianBlur(gray_img, (11,11),0)
+            # qr_img = keras.preprocessing.image.load_img(img_filepath ,grayscale=True,target_size=(image_size,image_size))
+            qr_imgcv = cv2.imread(img_filepath)
+            # gray_img = cv2.cvtColor(qr_img, cv2.COLOR_BGR2GRAY)
+            gray_imgcv = cv2.cvtColor(qr_imgcv, cv2.COLOR_BGR2GRAY)
+            blurred_img = cv2.GaussianBlur(gray_imgcv, (11,11),0)
             barcodes = pyzbar.decode(blurred_img)
             
     
@@ -62,13 +66,13 @@ def upload_file():
             i=1
 
             ans_correct  = 0
-
+            qr_list = np.empty((1,3))
             for  barcode in barcodes:
                 (x, y, w, h) = barcode.rect
                 #以下imwriteは確認
                 #binary image 
                 barcodeData = barcode.data.decode("utf-8")
-                crop_cut = gray_img[y:y+h ,x+310 :x+w+310]
+                crop_cut = gray_imgcv[y:y+h ,x+310 :x+w+310]
                 th, binary = cv2.threshold(crop_cut,125, 255, cv2.THRESH_BINARY)
                 #cv2.imwrite('crop_cut_binary_{}.jpg'.format(i),binary)
 
@@ -78,7 +82,7 @@ def upload_file():
 
                 
                 _, thresh = cv2.threshold(binary, 125, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-                image, contours, hierarchy = cv2.findContours(thresh, 3, cv2.CHAIN_APPROX_NONE)
+                contours, hierarchy = cv2.findContours(thresh, 3, cv2.CHAIN_APPROX_NONE)
                 cnt = contours[0]
                 X, Y, W, H = cv2.boundingRect(cnt) 
                 rectangle = binary2black[Y:Y+H,X:X+W]
@@ -105,15 +109,20 @@ def upload_file():
                     ans_correct += 1
                 else:
                     print('The answer is incorrect')
-
+                qr_list = np.append(qr_list, [[qr_que, qr_ans, true_ans]], axis=0)
         
+        qr_list = np.delete(qr_list, 0, 0)
+        qr_list = qr_list[qr_list[:,0].argsort(), :]
         x1 = (ans_correct / 10.0) * 100
         print('If the prediction is 100% correct. The correct rate of answer is {} %'.format(x1))
-                        
+        return render_template('result.html', res_list=qr_list)                
     #img_read = cv2.imread('test2.jpg')
     #read_pred(img_read)
+    return render_template('index.html', answer = '')
 
-
+#直接実行した時のみに動く
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
 
 
 
